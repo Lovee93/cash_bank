37f276361329b0240ccee6fcebcb841b1544de10
Lovee93 <lovee.jain93@gmail.com>
2020-03-28 20:25:03 +1100

    Updates

---
+++ precache-manifest.9f5c156040d04cc1936ef72e7da9de79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff38261edccd9867bf015fbd4f9cb4f",
+    "url": "/cash_bank/index.html"
+  },
+  {
+    "revision": "b5c33d46697595cd3911",
+    "url": "/cash_bank/static/css/main.6dfcd059.chunk.css"
+  },
+  {
+    "revision": "3687ba0f7a9389e5ccb3",
+    "url": "/cash_bank/static/js/2.f0743e0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cash_bank/static/js/2.f0743e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c33d46697595cd3911",
+    "url": "/cash_bank/static/js/main.e021d62f.chunk.js"
+  },
+  {
+    "revision": "82d20f9510c351f47785",
+    "url": "/cash_bank/static/js/runtime-main.1a6c0238.js"
+  }
+]);
